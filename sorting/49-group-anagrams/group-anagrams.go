5e5b31d53feb1b42bd42c34c7e1b901524f57510
ahummani <150867065+ahummani@users.noreply.github.com>
2023-11-15 06:27:38 +0530

    Time: 23 ms (50.98%) | Memory: 8.1 MB (48.94%) - LeetSync

--- sorting/49-group-anagrams/group-anagrams.go
+++ sorting/49-group-anagrams/group-anagrams.go
@@ -1,23 +1,18 @@
-type Key [26]byte
-
-func strKey(str string) (key Key) {
-    for i := range str {
-        key[str[i]-'a']++
-    }
-    return
-}
-
 func groupAnagrams(strs []string) [][]string {
-    groups := make(map[Key][]string)
-
-    for _,v := range strs {
-        key := strKey(v)
-        groups[key] = append(groups[key],v)
-    }    
-
-    result := make([][]string, 0, len(groups))
-    for _, v := range groups {
-        result = append(result, v)
+    mapS := make(map[string][]string, 0)
+    
+    for _, val := range strs {
+        byteString := []byte(val)
+        sort.Slice(byteString, func(i,j int) bool { return byteString[i] < byteString[j]})
+        sortedString := string(byteString)
+        
+        mapS[sortedString] = append(mapS[sortedString], val)
     }
-    return result
+    
+    ans := make([][]string,0)
+
+    for _,val := range mapS {
+        ans = append(ans, val)
+    }
+    return ans
 }
