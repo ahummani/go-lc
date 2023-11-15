a20bc0a76ba570710c570885ba9e014cfa8a89fd
ahummani <150867065+ahummani@users.noreply.github.com>
2023-11-15 19:12:53 +0530

    Time: 16 ms (81.09%) | Memory: 6.6 MB (78.70%) - LeetSync

--- string/56-merge-intervals/merge-intervals.go
+++ string/56-merge-intervals/merge-intervals.go
@@ -1,21 +1,33 @@
 func merge(intervals [][]int) [][]int {
-    res := make([][]int, 0)
+	mp := make(map[int]int)
+	var start int
+	var end int
+	for i := 0; i < len(intervals); i++ {
+		start = intervals[i][0]
+		end = intervals[i][1]
+		mp[start] += 1
+		mp[end] -= 1
+	}
+	var keys []int
+	for key := range mp {
+		keys = append(keys, key)
+	}
 
-    sort.Slice(intervals, func(i,j int) bool {
-        return intervals[i][0]<intervals[j][0]
-    })
+	sort.Ints(keys)
+	start = 0
+	count := 0
+	var answer [][]int
+	var value int
+	for _, key := range keys {
+		value = mp[key]
+		if count == 0 {
+			start = key
+		}
+		count += value
 
-    res = append(res, intervals[0])
-
-
-    for _, interval := range intervals[1:] {
-        if res[len(res)-1][1]>=interval[0] {
-            if res[len(res)-1][1] <interval[1] {
-                res[len(res)-1][1] = interval[1]
-            }
-            continue
-        }
-        res = append(res, interval)
-    }  
-    return res
+		if count == 0 {
+			answer = append(answer, []int{start, key})
+		}
+	}
+	return answer
 }
