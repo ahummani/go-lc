a57c35c4ac6efdafc42414efec018ae2c6d38052
ahummani <150867065+ahummani@users.noreply.github.com>
2023-11-17 04:22:31 +0530

    Time: 36 ms (6.90%) | Memory: 3.7 MB (8.05%) - LeetSync

---
+++ sorting/147-insertion-sort-list/insertion-sort-list.go
@@ -0,0 +1,16 @@
+func insertionSortList(head *ListNode) *ListNode {
+    arr := []*ListNode{}
+    cur := head
+    
+    for cur != nil {
+        if len(arr) != 0 && cur.Val < arr[len(arr) - 1].Val {
+            cur.Val, arr[len(arr) - 1].Val = arr[len(arr) - 1].Val, cur.Val
+            cur = arr[len(arr) - 1]
+            arr = arr[:len(arr) - 1]
+        } else {
+            arr = append(arr, cur)
+            cur = cur.Next
+        }
+    }
+    return head
+}
